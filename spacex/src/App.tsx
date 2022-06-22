c353870d0be48cef7b2d955ab4e8d2b6131b62f5
Sayah Reda <reda1998.rs@gmail.com>
2022-06-22 16:01:35 +0100

    move in direction  logic

--- spacex/src/App.tsx
+++ spacex/src/App.tsx
@@ -29,6 +29,141 @@ const isThereObstacle = (x:Number, y:number) => {
   }
   return false;
 };
+  // Check if the provided coordinate is within the grid's bounds.
+// Returns a Boolean
+const isInGrid = (x:number, y:number) => {
+  if (x < 0 || y < 0 || x > 12   || y > 12) {
+    return false;
+  }
+  return true;
+}
+// Check if a player can move to the provided coordinates.
+// Returns a Boolean
+const canMoveTocoordinates = (x:number, y:number) => {
+  // If the coordinate to move is outside of the grid,
+  // the player can't move to it.
+  if (!isInGrid(x, y)) {
+    return false;
+  }
+  // If there is a rock at the coordinate, 
+  // the player can't move to it.
+  if (isThereObstacle(x, y)) {
+    return false;
+  }
+  return true;
+};
+// Move the character to an x,y grid coordinate.
+// Returns nothing
+const moveCharacterTo = (x:number, y:number) => {
+
+  // Multiply the coordinates by 100 because each grid square
+  // is 100x100 pixels in size.
+ if  (null !== character.current) {
+  character.current.style.top = (y * 100).toString() + 'px';
+  character.current.style.left = (x * 100).toString() + 'px';
+ 
+
+};
+}
+// Move the character left one tile, if Blerf can.
+const turnLeft = () => {
+  if(direction==="right")
+  {
+      SetDirection("top")
+  }
+  else  if(direction==="bottom"){
+    SetDirection("right")
+  }
+  else  if(direction==="top"){
+    SetDirection("left")
+  }
+  else if(direction==="left"){
+    SetDirection("bottom")
+  }
+  console.log(direction)
+
+
+}
+
+// Move the character right one tile, if Blerf can.
+const turnRight = () => {
+  if(direction==="right")
+  {
+      SetDirection("bottom")
+  }
+  else  if(direction==="bottom"){
+    SetDirection("left")
+  }
+  else  if(direction==="left"){
+    SetDirection("top")
+  }
+  else  if(direction==="top"){
+    SetDirection("right")
+  }
+ 
+
+}
+
+// Move the character up one tile, if Blerf can.
+const moveUp = () => {
+  if(direction==="right")
+  {
+    if (canMoveTocoordinates(characterPos.x + 1, characterPos.y)) {
+      characterPos.x += 1;
+      moveCharacterTo(characterPos.x, characterPos.y);
+    }
+  }
+  else  if(direction==="bottom"){
+    if (canMoveTocoordinates(characterPos.x, characterPos.y + 1)) {
+      characterPos.y += 1;
+      moveCharacterTo(characterPos.x, characterPos.y);
+    }
+  }
+  else  if(direction==="left"){
+    if (canMoveTocoordinates(characterPos.x - 1, characterPos.y)) {
+      characterPos.x -= 1;
+      moveCharacterTo(characterPos.x, characterPos.y);
+    }
+  }
+  else if(direction==="top"){
+    if (canMoveTocoordinates(characterPos.x, characterPos.y - 1)) {
+      characterPos.y -= 1;
+      moveCharacterTo(characterPos.x, characterPos.y);
+    }
+  }
+
+};
+
+// Move the character down one tile, if Blerf can.
+const moveDown = () => {
+  if(direction==="left")
+  {
+    if (canMoveTocoordinates(characterPos.x + 1, characterPos.y)) {
+      characterPos.x += 1;
+      moveCharacterTo(characterPos.x, characterPos.y);
+    }
+  }
+  else  if(direction==="top"){
+    if (canMoveTocoordinates(characterPos.x, characterPos.y + 1)) {
+      characterPos.y += 1;
+      moveCharacterTo(characterPos.x, characterPos.y);
+    }
+  }
+  else  if(direction==="right"){
+    if (canMoveTocoordinates(characterPos.x - 1, characterPos.y)) {
+      characterPos.x -= 1;
+      moveCharacterTo(characterPos.x, characterPos.y);
+    }
+  }
+  else  if(direction==="bottom"){
+    if (canMoveTocoordinates(characterPos.x, characterPos.y - 1)) {
+      characterPos.y -= 1;
+      moveCharacterTo(characterPos.x, characterPos.y);
+    }
+  }
+};
+
+
   return (
 <div className="page-wrapper">
 
