a4508d221807b7d5a37d75313aeaf4810089bcc0
Sayah Reda <reda1998.rs@gmail.com>
2022-06-22 15:52:11 +0100

    Add:Layout of grid

--- spacex/src/App.tsx
+++ spacex/src/App.tsx
@@ -1,18 +1,29 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-
-
 function App() {
+  const character=useRef<HTMLDivElement>(null)
+  const board=useRef<HTMLDivElement>(null)
+  const [characterPos,SetcharacterPos]=useState( {
+    x: 0,
+    y: 0
+  })
+  const [rocks,Setrocks]=useState([
+    {x: 1, y: 1},
+    {x: 2, y: 2},
+    {x: 2, y: 3},
+    {x: 5, y: 5}
+  ])
+  const [direction,SetDirection]=useState("right")
   return (
-    <div className="App">
-      <div className="container">
-          <div className="board" id="board">
-          
-          </div>
+<div className="page-wrapper">
+
+<div className="board" ref={board}>
      
+        <div className={"character " }ref={character}></div>
       </div>
+
     </div>
   );
 }
