7c6f1b3a8e85b874d1586e66c97aada5434eede0
Sayah Reda <reda1998.rs@gmail.com>
2022-06-24 14:17:41 +0100

    add the basket coponent

--- basket/src/App.tsx
+++ basket/src/App.tsx
@@ -1,6 +1,8 @@
 import { Fragment, useEffect, useState } from 'react'
 import { Dialog, Popover, Tab, Transition } from '@headlessui/react'
 import { MenuIcon, QuestionMarkCircleIcon, SearchIcon, ShoppingBagIcon, XIcon } from '@heroicons/react/outline'
+import {  TrashIcon } from '@heroicons/react/solid'
+import { PlusSmIcon as PlusSmIconSolid } from '@heroicons/react/solid'
 
 
 const currencies = ['CAD', 'USD', 'AUD', 'EUR', 'GBP']
@@ -57,6 +59,7 @@ export default function Example() {
   const [Active,SetActive]=useState(false)
   const [open, setOpen] = useState(false)
 
+
   const  [products,Setproducts]=useState( [
     {
       id: 1,
@@ -97,6 +100,9 @@ export default function Example() {
     
    
   ])
+  let items=products;
+  let tot=0;
+  let totwd=0;
   return(
     <div className="bg-gray-50">
             <Transition.Root show={open} as={Fragment}>
@@ -463,6 +469,110 @@ export default function Example() {
         </nav>
       </header>
       <main  className="max-w-7xl mx-auto pt-16 pb-24 px-4 sm:px-6 lg:px-8 relative">
+      { Active && (
+    <div className="mt-10 lg:mt-0 absolute w-[500px] right-0 z-0" >
+        
+
+    <div className="mt-4 bg-white border border-gray-200 rounded-lg shadow-sm">
+      <h3 className="sr-only">Items in your cart</h3>
+      <ul role="list" className="divide-y divide-gray-200">
+        {products.map((product) => (
+          product.qty>0 && <li key={product.id} className="flex py-6 px-4 sm:px-6">
+            <div className="flex-shrink-0">
+              <img src={product.imageSrc} alt={product.imageAlt} className="w-20 rounded-md" />
+            </div>
+
+            <div className="ml-6 flex-1 flex flex-col">
+              <div className="flex">
+                <div className="min-w-0 flex-1">
+                  <h4 className="text-sm">
+                    <a href={product.href} className="font-medium text-gray-700 hover:text-gray-800">
+                      {product.name}
+                    </a>
+                  </h4>
+                  <p className="mt-1 text-sm text-gray-500">{product.color}</p>
+                  <p className="mt-1 text-sm text-gray-500">{product.size}</p>
+                </div>
+
+                <div className="ml-4 flex-shrink-0 flow-root">
+                  <button
+                    type="button"
+                    onClick={()=>{
+                      product.qty=0;
+                      Setproducts([...products])
+                      Setbasket(Basket-1)
+                    }}
+                    className="-m-2.5 bg-white p-2.5 flex items-center justify-center text-gray-400 hover:text-gray-500"
+                  >
+                    <span className="sr-only">Remove</span>
+                    <TrashIcon className="h-5 w-5" aria-hidden="true" />
+                  </button>
+                </div>
+              </div>
+
+              <div className="flex-1 pt-2 flex items-end justify-between">
+                <p className="mt-1 text-sm font-medium text-gray-900">{product.Discount.toFixed(2)} $</p> <p className='mt-1 text-sm font-medium text-red-700 line-through'>{(product.qty*product.price)-product.Discount>0.2 ?( product.price*product.qty).toFixed(2) :""}</p>
+
+                <div className="ml-4 flex items-center gap-5">
+                <button
+                  onClick={()=>{
+                    product.qty=product.qty-1;
+                    Setproducts([...products])
+                  }}
+    type="button"
+    className="inline-flex items-center p-1 border border-transparent rounded-full shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+  >
+    <PlusSmIconSolid className="h-5 w-5" aria-hidden="true" />
+  </button>
+  <span className=' text-xl font-bold '>{product.qty}</span>
+  <button
+      onClick={()=>{
+        product.qty=product.qty+1;
+        Setproducts([...products])
+        
+      }}
+    type="button"
+    className="inline-flex items-center p-1 border border-transparent rounded-full shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+  >
+    <PlusSmIconSolid className="h-5 w-5" aria-hidden="true" />
+  </button>
+
+                </div>
+              </div>
+            </div>
+          </li>
+        ))}
+      </ul>
+      <dl className="border-t border-gray-200 py-6 px-4 space-y-6 sm:px-6">
+        <div className="flex items-center justify-between">
+          <dt className="text-sm">Subtotal</dt>
+          <dd className="text-sm font-medium text-gray-900">${totwd.toFixed(2)}</dd>
+        </div>
+        <div className="flex items-center justify-between">
+          <dt className="text-sm">Shipping</dt>
+          <dd className="text-sm font-medium text-gray-900">$5.00</dd>
+        </div>
+        <div className="flex items-center justify-between">
+          <dt className="text-sm">Dicount</dt>
+          <dd className="text-sm font-medium text-gray-900">${totwd-tot}</dd>
+        </div>
+        <div className="flex items-center justify-between border-t border-gray-200 pt-6">
+          <dt className="text-base font-medium">Total</dt>
+          <dd className="text-base font-medium text-gray-900">$ {tot.toFixed(2)}</dd>
+        </div>
+      </dl>
+
+      <div className="border-t border-gray-200 py-6 px-4 sm:px-6">
+        <button
+          type="submit"
+          className="w-full bg-indigo-600 border border-transparent rounded-md shadow-sm py-3 px-4 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-50 focus:ring-indigo-500"
+        >
+          Confirm order
+        </button>
+      </div>
+    </div>
+  </div>
+)}
 
         <div className="max-w-2xl mx-auto lg:max-w-none" onClick={()=>{SetActive(false)}}>
           <h1 className="sr-only">Checkout</h1>
